a5980c2e90a88f9bf10c0f94df32f92af4c5a5ea
David Peters <57600507+peters-david@users.noreply.github.com>
2022-03-06 09:46:04 +0100

    updated zeroable to accept bigger values

--- src/zeroable.rs
+++ src/zeroable.rs
@@ -117,7 +117,7 @@ unsafe impl<T, const N: usize> Zeroable for [T; N] where T: Zeroable {}
 impl_unsafe_marker_for_array!(
   Zeroable, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
   19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 48, 64, 96, 128, 256,
-  512, 1024, 2048, 4096
+  512, 1024, 2048, 4096, 8192, 16384, 32768
 );
 
 #[cfg(all(target_arch = "wasm32", feature = "wasm_simd"))]
